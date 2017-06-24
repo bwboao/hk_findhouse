a9a75a42fd16ca0232abe2ca9e90551f1f4344f2
bwb <ben3226@gmail.com>
2017-06-24 12:43:58 +0800

    first commit build-up env

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# webpack stuffs
+npm install webpack --save-dev 
+npm install webpack-dev-server --save-dev
+npm install webpack-hot-middleware --save-dev
+npm install webpack-notifier --save-dev 
+npm install html-webpack-plugin --save-dev 
+
+# install lots of loaders
+npm install url-loader file-loader sass-loader css-loader postcss-loader --save
+
+# babel stuffs
+npm install babel-plugin-react-transform --save-dev
+npm install babel-loader babel-core --save-dev
+npm install babel-preset-es2015 babel-preset-react --save-dev
+
+# react and redux stuffs
+npm install redux react-redux --save
+npm install react-tap-event-plugin --save
+npm install react react-dom --save
+npm install react-transform-hmr --save-dev
+# react-router
+npm install react-router --save
+
+# some other stuffs
+# file loader
+npm install file-loader --save
+
